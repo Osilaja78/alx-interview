0b90499cb18c3e9a00eb2b1e3ebdf15454ed652a
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-01-06 14:59:12 +0100

    added file for task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains a code that returns a pascal's
+triangle based on n.
+"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for _ in range(1, n):
+        row = [1]
+        for j in range(1, len(triangle[-1])):
+            row.append(triangle[-1][j - 1] + triangle[-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
