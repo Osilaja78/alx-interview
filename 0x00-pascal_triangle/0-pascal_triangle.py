f3c447657f052edc95b6f0b3422635287b2d2a06
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-01-06 15:07:33 +0100

    added docstring to pascal_triangle function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
+
 def pascal_triangle(n):
+    """
+    This function returns a list of lists of integers
+    representing the Pascal’s triangle of n:
+    """
+
     if n <= 0:
         return []
 
